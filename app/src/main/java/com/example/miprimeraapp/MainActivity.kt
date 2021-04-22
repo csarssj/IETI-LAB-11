cf718abb9c59a5db207eb8112a35cf196820bc0a
cesar eduardo gonzalez cuentas <cesar.gonzalez-c@mail.escuelaing.edu.co>
2021-04-22 15:40:39 -0500

    :sparkles: laboratorio finalizado

---
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.miprimeraapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock
+import android.view.View
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
